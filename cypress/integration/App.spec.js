a6e2492b570fdfea665705b1b0c38c8c0f7fd02a
Jenna Rajani <jjrajani@gmail.com>
2020-08-16 14:41:06 -0400

    add failing tests

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -13,6 +13,10 @@ context("App", () => {
     cy.get(".logo").should("exist");
   });
 
+  it("Dispalys the logo", () => {
+    cy.get(".logo").should("exist");
+  });
+
   it("Dispalys a link to react docs", () => {
     cy.get("a")
       .should("have.attr", "href")
